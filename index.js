cd50571f28062d3b21dc8a88c4afedd84c966fec
long <chuthanhlong2k2@gmail.com>
2023-10-16 02:22:00 +0700

    second

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const links = document.querySelectorAll('.navbar__link');
+const sections = document.querySelectorAll('section');
 
 links.forEach((link) => {
    link.addEventListener('click', (e) => {
-      const linkActive = document.querySelector('.navbar__link.active');
-      linkActive.classList.remove('active');
+      links.forEach((link) => link.classList.remove('active'));
 
       e.target.classList.add('active');
+   });
+});
+
+window.addEventListener('scroll', () => {
+   const heightWindow = window.scrollY;
+   const heightClient = document.documentElement.clientHeight;
+
+   links.forEach((item, index) => {
+      if (heightWindow > heightClient * index - heightClient / 2) {
+         links.forEach((link) => link.classList.remove('active'));
+         item.classList.add('active');
+      }
    });
 });
